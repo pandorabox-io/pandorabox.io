8dc190cc51403c58ca131178699cd1bef44558b6
Thomas Rudin <thomas@rudin.io>
2019-06-12 20:10:23 +0200

    export whole block db with player data

--- create-export.sh
+++ create-export.sh
@@ -2,7 +2,7 @@
 
 mkdir export
 
-docker-compose exec postgres pg_dump -U postgres postgres -t blocks | gzip > export/blocks.sql.gz
+docker-compose exec postgres pg_dump -U postgres postgres | gzip > export/blocks.sql.gz
 
 cd data/minetest/world && tar cvjf ../../../export/world.tar.bz2 --exclude .git --exclude monitoring --exclude webmail advtrains* areas.dat atm_* beds_spawns datastorage/ elevator env_meta.txt force_loaded.txt locator_beacons.txt map_meta.txt mod_travelnet.data planets.json pvp_areas_store.dat teleport_tubes world.mt worldmods*
 
