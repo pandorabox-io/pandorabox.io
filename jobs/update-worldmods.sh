6cb94961e597e4e3bc24b4baa31dfd4244dde063
Thomas Rudin <thomas@rudin.io>
2019-12-09 13:44:14 +0100

    worldmod updater in container

--- jobs/update-worldmods.sh
+++ jobs/update-worldmods.sh
@@ -3,20 +3,26 @@
 cd $(dirname $0)
 cd ../data/minetest/world/worldmods
 
-git remote update
-git diff --exit-code HEAD origin/master && exit 0
+while `true`
+do
+	sleep 60
 
-git pull
+	git remote update
+	git diff --exit-code HEAD origin/master && continue
 
-# new mods
-git submodule init
+	git pull
 
-# changed repo urls
-git submodule sync
+	# new mods
+	git submodule init
+	
+	# changed repo urls
+	git submodule sync
 
-# updated commits
-git submodule update
+	# updated commits
+	git submodule update
 
-# removed directories
-git clean -dff
+	# removed directories
+	git clean -dff
+
+done
 
