4a1c9ee4c898f6e5f8b52b31051dfed94e329013
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-12 18:06:37 +0200

    remove pipeworks luaentities on engine upgrade
    fixes https://github.com/pandorabox-io/pandorabox.io/issues/483

--- update-minetest-image.sh
+++ update-minetest-image.sh
@@ -4,5 +4,7 @@ echo "say Maintenance-restart, sorry for the interruption" > data/minetest/world
 sleep 5
 echo "shutdown 120 true maintenance" > data/minetest/world/shell.txt
 sleep 110
+docker-compose stop minetest
+rm data/minetest/world/luaentities
 docker-compose up -d minetest
 
