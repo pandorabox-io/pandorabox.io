8942239c116b53798d7f0167b80ee8c1f0932b64
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2020-09-09 17:48:10 +0200

    only export "blocks" table / exclude certain directories

--- create-export.sh
+++ create-export.sh
@@ -5,11 +5,17 @@ mkdir export
 # export blocks and wiki-db as gzipped sql dump
 docker-compose up -d postgres
 sleep 10
-docker-compose exec postgres pg_dump -U postgres postgres | gzip > export/blocks.sql.gz
+docker-compose exec postgres pg_dump -U postgres -t blocks postgres | gzip > export/blocks.sql.gz
 docker-compose exec postgres pg_dump -U postgres -n mediawiki wiki | gzip > export/wiki.sql.gz
 docker-compose down
 
 # tar archive of the world files
-cd data/minetest/world && tar cvjf ../../../export/world.tar.bz2 --exclude mails --exclude mapserver.tiles --exclude mapserver.json *
+cd data/minetest/world && tar cvjf ../../../export/world.tar.bz2 \
+ --exclude mails \
+ --exclude mapserver.tiles \
+ --exclude mapserver.json \
+ --exclude xban.db \
+ --exclude mod_storage *
+
 # tar archive of the mediawiki
 cd data/wiki && tar cvjf ../../export/wiki.tar.bz2 --exclude LocalSettings.secrets.php *
