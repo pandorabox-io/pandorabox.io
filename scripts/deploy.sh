e38d52fedd3c6c4af07dca6a6c0e79e1c384efc3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-15 19:22:37 +0100

    serial deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # deployment script for automated updates via github actions
 
+# ensure exclusive execution with flock
+LOCK_FILE=/tmp/pandorabox-deployment.lock
+exec 200>"$LOCK_FILE"
+flock -n 200 || exit
+
 # get latest repo version
 git pull
 
