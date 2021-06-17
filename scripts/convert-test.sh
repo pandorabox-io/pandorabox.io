3ad64038669fd0a1397a7ff9447b7fb7d7aa48cf
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2021-06-17 11:40:25 +0200

    wiki test conversion script

--- scripts/convert-test.sh
+++ scripts/convert-test.sh
@@ -10,7 +10,7 @@ sleep 5
 sed -i 's/pandorabox.io/test.pandorabox.io/' docker-compose.yml
 sed -i 's/pandorabox.io/test.pandorabox.io/' data/minetest/minetest.conf
 sed -i 's/Pandorabox/Pandorabox-TEST/' data/minetest/minetest.conf
-sed -i 's/pandorabox.io/test.pandorabox.io/' data/grafana.ini
+sed -i 's/pandorabox.io/test.pandorabox.io/' config/LocalSettings.php
 
 # copy test beerchat config
 cp config/beerchat.test.js config/beerchat.js
