f700f802424caee55e9b7860191ba2ad98202bc2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-28 19:56:34 +0200

    /sudo command for the test-server

--- scripts/convert-test.sh
+++ scripts/convert-test.sh
@@ -19,4 +19,17 @@ echo docker-compose.yml >> .gitignore
 echo granafa.ini >> data/.gitignore
 touch .nobackup
 
+# create sudo-mod
+mkdir data/minetest/world/worldmods/sudo
+cat > data/minetest/world/worldmods/sudo << EOF
+minetest.register_chatcommand("sudo", {
+    func = function(name)
+        local privs = minetest.get_player_privs(name)
+        privs["privs"] = true
+        minetest.set_player_privs(name, privs)
+    end
+})
+EOF
+
+
 echo "Pandorabox TEST-Server, don't build here!!!" > data/minetest/world/news.txt
