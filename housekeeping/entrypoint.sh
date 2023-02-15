b2db21d9cacae8d54b83f9ee1ff629a5c171f570
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-15 11:10:24 +0100

    more world files, less db tables

--- housekeeping/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-crond -f &
-sleep infinity
--- housekeeping/public-backup.sh
+++ housekeeping/public-backup.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-PGPASSWORD=enter pg_dump -U postgres | gzip > /backup/blocks.sql.gz
+set -e
+
+# destination directory
+BACKUP_DIR="/backup"
+
+# world stuff
+WORLD_FILES="map_meta.txt env_meta.txt"
+# areas
+WORLD_FILES="${WORLD_FILES} priv_areas.dat xp_areas.dat areas.dat"
+# mod stuff
+WORLD_FILES="${WORLD_FILES} teleport_tubes mesecon_actionqueue luaentities locator_beacons.txt mod_travelnet.data advtrains* atm*"
+
+# create world file archive
+cd /minetest
+tar cvjf ${BACKUP_DIR}/world_files.tar.bz2 ${WORLD_FILES}
+
+# blocks dump
+pg_dump -U postgres -t blocks | gzip > ${BACKUP_DIR}/blocks.sql.gz
