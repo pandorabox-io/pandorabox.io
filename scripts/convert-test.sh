d4a04981ac6ea22896209104b175e9a823641db9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-05-17 15:29:36 +0200

    matterbridge test config in conversion script

--- scripts/convert-test.sh
+++ scripts/convert-test.sh
@@ -9,8 +9,8 @@ sed -i 's/Pandorabox/Pandorabox-TEST/' data/minetest/minetest.conf
 sed -i 's/server_announce = true/server_announce = false/' data/minetest/minetest.conf
 sed -i 's/pandorabox.io/test.pandorabox.io/' config/LocalSettings.php
 
-# copy test beerchat config
-cp config/beerchat.test.js config/beerchat.js
+# copy test matterbridge config
+cp config/matterbridge.test.toml config/matterbridge.toml
 
 echo docker-compose.yml >> .gitignore
 echo granafa.ini >> data/.gitignore
