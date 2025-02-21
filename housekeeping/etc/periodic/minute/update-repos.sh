b7ee983a60b1b358da1f88aa9346533a8bb55c93
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-02-21 10:02:49 +0100

    cleanup housekeeping structure, skip `run-parts`

--- housekeeping/etc/periodic/minute/update-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-update-git /data/minetest/world/worldmods master >> /data/update-log.txt &
-update-git /data/pandorabox-textures/ master >> /data/update-log.txt &
-update-git /data/minetest/world/worldmods/skinsdb/textures master >> /data/update-log.txt &
